4d65294fc5acf3e4c72dd865276c49317dcb8cc1
rupamShaw1998 <rupamshw77777@gmail.com>
2024-02-17 22:01:24 +0530

    feat: Add profile pic and name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { Typography } from 'antd';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Container, IMG } from './App.styled';
+
+const { Title } = Typography;
 
 function App() {
 
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = async () => {
+    let userId = "u-a2399489-9cd0-4c94-ad12-568379202b08";
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        apikey: 'k-6fe7e7dc-ac8f-44a1-8bbf-a1754ddf88be',
+        userid: 'u-a2399489-9cd0-4c94-ad12-568379202b08',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1LWEyMzk5NDg5LTljZDAtNGM5NC1hZDEyLTU2ODM3OTIwMmIwOCIsImlhdCI6MTcwNzk4NzYyOSwiZXhwIjoxNzA4NTkyNDI5fQ.fESDqKunAqLUgHBCUsNYpGcNrTeVEty91HqGebX59Uc'
+      }
+    };
+    try {
+      let res = await fetch(`https://staging.questprotocol.xyz/api/users/${userId}`, options);
+      let response = await res.json();
+      let { data: { imageUrl, name }} = response;
+      setProfile({ imageUrl, name });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <>
-      Quest Labs
-    </>
+    <div>
+      <Title style={{ color: "white" }} level={4}>Profile</Title>
+      <Container>
+        <IMG src={profile.imageUrl} alt="Profile Pic" />
+        <Title level={3}>{profile.name}</Title>
+      </Container>
+    </div>
   );
 }
 
---
+++ src/App.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: white;
+  position: absolute;
+  z-index: 5;
+  width: 90%;
+  top: 20%;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+  border-radius: 5% 5% 0 0;
+`;
+
+const IMG = styled.img`
+  width: 27%;
+  margin-top: -14%;
+  border-radius: 50%;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+export { Container, IMG };
